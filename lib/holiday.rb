fecf2770026ddea8704e44e7e3ed7570094470b2
kowalix007 L.K <kowalix007@gmail.com>
2020-06-24 14:25:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
     cap_season = season.to_s.capitalize!
     puts "#{cap_season}:"
     holidays.each do |holiday,supplies|
-    holiday_string = holiday.to_s.split("_").map{|word| word.capitalize!}.join
+    holiday_string = holiday.to_s.split("_").map{|word| word.capitalize!}.join(" ")
     supplies_string = supplies.join(", ") #changes to str.
     puts "  #{holiday_string}: #{supplies_string}"
  end 
